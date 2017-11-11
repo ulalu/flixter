034cd84bd04a48755bd9ba6bf91d7ca57e9a445e
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-11-11 18:37:02 +0000

    added user authentification for course material access in lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :user_authentification
+    
     def show
     end
     
@@ -8,5 +11,12 @@ class LessonsController < ApplicationController
     def current_lesson
         @current_lesson ||= Lesson.find(params[:id])
     end
+    
+    def user_authentification
+        if ! current_user.enrolled_in?(current_lesson.section.course)
+            redirect_to course_path(current_lesson.section.course), alert: 'Please enroll in order to view course material'
+        end
+    end
+    
         
 end
